d7d5347979e4440521ad652503439e568f8cfd2d
Just <grivten@yandex.ru>
2023-03-02 00:45:32 +0300

    redraw gui

--- src/main/java/net/just_s/rpupdater/util/RPVisibilityButton.java
+++ src/main/java/net/just_s/rpupdater/util/RPVisibilityButton.java
@@ -7,8 +7,8 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 
 public class RPVisibilityButton extends TexturedButtonWidget {
-    private int this_x;
-    private int this_y;
+    private final int this_x;
+    private final int this_y;
     public RPVisibilityButton(int x, int y, PressAction pressAction) {
         super(x, y, 20, 20, 0, 0, ModTextures.VISIBILITY_BTN, pressAction);
         this_x = x;
@@ -21,11 +21,17 @@ public class RPVisibilityButton extends TexturedButtonWidget {
 
         // Render the current session status
         RenderSystem.setShaderTexture(0, ModTextures.VISIBILITY_BTN);
-        final int v;
-        if (RPUpdMod.CONFIG.shouldRenderInMenu()) {
-            v = 0;
-        } else
+        int v;
+        if (this.hovered) {
             v = 20;
-        drawTexture(matrices, this_x, this_y, 0, v, 20, 20, 20, 40);
+        } else {
+            v = 0;
+        }
+        drawTexture(matrices, this_x, this_y, 0, v, 20, 20, 40, 40);
+        if (RPUpdMod.CONFIG.shouldRenderInMenu())
+            v = 0;
+        else
+            v = 10;
+        drawTexture(matrices, this_x + 2, this_y + 5, 20, v, 18, 10, 40, 40);
     }
 }
